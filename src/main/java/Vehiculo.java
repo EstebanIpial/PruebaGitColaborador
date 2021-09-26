cd75e9871b73fe265c3a3b968312712f3b5944e1
esteb <esteb@LAPTOP-3LJSCG9I>
2021-09-26 18:56:42 -0500

    //se le implemento la clase vehiculo

---
+++ src/main/java/Vehiculo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author esteb
+ */
+public class Vehiculo {
+    private String marca;
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    
+    
+    
+}
